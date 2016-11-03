92559b886702040afe76d7220ded780ffb528c0d
Blake Hagen <blakehagen@gmail.com>
2016-11-02 21:56:36 -0600

    playing with countdown timer

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import Countdown from 'components/Countdown';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <div>Hello World!</div>
-    )
+    return <Countdown/>
   }
 }
