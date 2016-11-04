1013fd03fad2ce521b433f2c9aa71496d1902653
Blake Hagen <blakehagen@gmail.com>
2016-11-03 20:00:03 -0600

    updated pic

--- app/components/Countdown/Counter.jsx
+++ app/components/Countdown/Counter.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import Countdown from 'react-count-down';
-import autoBind from 'react-autobind';
 import styles from './counter.scss';
 
 export default class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    autoBind(this);
-  }
-
   render() {
-    let OPTIONS = {endDate: '12/16/2016 10:00 AM'};
+    let countdownOptions = {endDate: '12/20/2016 9:00 AM'};
 
     return (
       <div className={styles.container}>
         <div className={styles.timerContainer}>
           <div className={styles.timer}>
-            <Countdown options={OPTIONS}/>
+            <Countdown options={countdownOptions}/>
           </div>
         </div>
       </div>
